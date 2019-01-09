56932abaa81a877cc4f1cadce4078d5b8107cc7d
efeng0414 <efeng@nobul.com>
2019-01-09 15:29:06 -0500

    add coments

--- src/scenes/home/home.test.js
+++ src/scenes/home/home.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Home from "./index";
 
+// Tesing
 describe("Home Component", () => {
   const componentJSX = <Home />;
 
