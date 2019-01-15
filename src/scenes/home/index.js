eb5118620ffcdcecbd36d0ac693b02fe093a5b51
efeng0414 <efeng@nobul.com>
2019-01-15 15:11:03 -0500

    update component

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Hello World from Edward at 2:31 pm!</div>;
+  return (
+    <div>
+      <h2>Hello World at 3:09 pm!</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quam
+        eros, luctus a metus eu, euismod pulvinar velit. Phasellus sagittis nisi
+        eget malesuada aliquet. Donec sapien orci, rutrum vel blandit eu,
+        scelerisque in metus. Sed in interdum tortor. Etiam suscipit condimentum
+        felis et bibendum. Praesent consequat pellentesque faucibus.
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Mauris scelerisque massa eget felis porta, sit
+        amet egestas ex tempor. In hac habitasse platea dictumst. Aliquam erat
+        volutpat. Duis aliquet dolor at elit auctor lacinia. Donec viverra sed
+        leo sed volutpat. Donec ut quam sagittis, mollis quam non, hendrerit mi.
+        Fusce vel urna ac nibh elementum scelerisque. Phasellus scelerisque
+        blandit tincidunt. Mauris neque risus, blandit et sapien vel, aliquam
+        porttitor dui. Vivamus vitae turpis turpis. Curabitur ullamcorper ligula
+        eu turpis iaculis, vel lobortis nisi vehicula. Vivamus posuere
+        ullamcorper volutpat. Maecenas in molestie sapien. Duis tincidunt semper
+        risus eget facilisis. Nulla in nulla ac magna euismod maximus. Fusce
+        maximus id mauris vitae rhoncus.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
